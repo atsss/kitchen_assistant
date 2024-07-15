4ea89ba4ae8a443a066b4d004c2e3f81e626d8df
atsss <atsushi0623itoh@gmail.com>
2024-07-15 10:23:09 +0100

    Evoke TTS from API endpoint (#6)
    * Create speak endpoit
    
    * Create TTS client class
    
    * Create __init__.py method
    
    * Add log
    
    * Pin numpy at the latest v1
    
    * Add missing self
    
    * Fix type
    
    * Change endpoint methodn and add request body
    
    * Add status code to response
    
    * Remove unecesary log

---
+++ src/classes/tts.py
@@ -0,0 +1,30 @@
+import os
+import pyaudio
+from piper import PiperVoice
+from loguru import logger
+
+class TTS:
+    PARENT_DIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
+    MODEL_PATH = os.path.join(PARENT_DIR, 'models', 'en_US-lessac-medium.onnx')
+    CONFIG_PATH = os.path.join(PARENT_DIR, 'models', 'en_US-lessac-medium.onnx.json')
+
+    def __init__(self) -> None:
+        self.piper = PiperVoice.load(self.MODEL_PATH, config_path=self.CONFIG_PATH)
+        self.audio = pyaudio.PyAudio()
+        self.stream = None
+
+    def __del__(self) -> None:
+        self.audio.terminate()
+
+    def start(self):
+        self.stream = self.audio.open(format=pyaudio.paInt16, channels=1, rate=22050, output=True)
+
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+
+    def speak(self, content: str) -> None:
+        logger.debug('Speaking')
+        for audio_data in self.piper.synthesize_stream_raw(content):
+            # Play audio as wirting audio data to stream
+            self.stream.write(audio_data)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from pydantic import BaseModel
+from .classes.tts import TTS
+
+class Message(BaseModel):
+    content: str
 
 app = FastAPI()
+tts_client = TTS()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.post("/speak")
+def root(message: Message):
+    # FIXME: Locate right after server starts
+    tts_client.start()
+    tts_client.speak(message.content)
+    # FIXME: Locate right before sever stops
+    tts_client.stop()
+    return Response(status_code=status.HTTP_200_OK)
