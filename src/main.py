4eaf0f600fafd32a78bb961140fab670dfd169b2
atsss <atsushi0623itoh@gmail.com>
2024-07-16 10:11:43 +0100

    Add middleware to log request body and header

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Request, Response, status
 from pydantic import BaseModel
+from loguru import logger
 from .classes.tts import TTS
 
 class Message(BaseModel):
@@ -7,6 +8,17 @@ class Message(BaseModel):
 
 app = FastAPI()
 tts_client = TTS()
+
+@app.middleware("http")
+async def log_request_body(request: Request, call_next):
+    body = await request.body()
+
+    logger.info(f"Request Headers: {request.headers}")
+    logger.info(f"Request Body: {body.decode()}")
+
+    # await request.body()
+
+    return await call_next(request)
 
 @app.get("/")
 def root():
