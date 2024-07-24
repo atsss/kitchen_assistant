c3d9170ae7ca1d5268d289c4e3e86debd775a207
atsss <atsushi0623itoh@gmail.com>
2024-07-24 10:09:59 +0100

    Subscribe slack event (#7)
    * Add slack endpoint
    
    * Add middleware to log request body and header
    
    * Add request body for slack subscription message
    
    * Add any type for slack message
    
    * Consider two types of message
    
    * Update body type
    
    * Add optional to challenge params
    
    * Remove slack suscription message model
    
    * Speak slack message

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Request, Response, status
 from pydantic import BaseModel
+from loguru import logger
+from typing import Dict, Any
 from .classes.tts import TTS
+import re
 
 class Message(BaseModel):
     content: str
@@ -8,15 +11,43 @@ class Message(BaseModel):
 app = FastAPI()
 tts_client = TTS()
 
+@app.middleware("http")
+async def log_request_body(request: Request, call_next):
+    body = await request.body()
+
+    logger.info(f"Request Headers: {request.headers}")
+    logger.info(f"Request Body: {body.decode()}")
+
+    # await request.body()
+
+    return await call_next(request)
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @app.post("/speak")
-def root(message: Message):
-    # FIXME: Locate right after server starts
+def speak(message: Message):
     tts_client.start()
     tts_client.speak(message.content)
-    # FIXME: Locate right before sever stops
     tts_client.stop()
+    return Response(status_code=status.HTTP_200_OK)
+
+@app.post("/slack")
+def slack(message: Dict[Any, Any]):
+    logger.info('Detected slack message')
+    if message.get('type', None) == 'url_verification':
+        logger.info('In verification')
+        return message.get('challenge', None)
+
+    text = message.get('event', {}).get('text', None)
+    logger.info(f"Slack text {text}")
+    if text is not None:
+        content = re.sub(r'<@U07DG05L1UY>\s*', '', text)
+        logger.info(f"Content to speack {content}")
+
+        tts_client.start()
+        tts_client.speak(content)
+        tts_client.stop()
+
     return Response(status_code=status.HTTP_200_OK)
